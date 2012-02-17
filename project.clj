0cbcb1838406e887850ad1a7cbac6907875c5066
Lee Hinman <lee@writequit.org>
2012-02-16 17:01:10 -0700

    Bump to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cld "0.1.0"
+(defproject cld "0.1.1-SNAPSHOT"
   :description "Clojure Language Detection"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.thnetos/langdetect-lib "1.1"]]
